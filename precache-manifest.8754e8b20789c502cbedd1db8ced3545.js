59fd2f264fa3f3deeac86e313d89f704ee55af83
valeriia.klimova <valeriia.klimova@x2sy.com>
2020-03-04 11:50:50 +0200

    Updates

---
+++ precache-manifest.8754e8b20789c502cbedd1db8ced3545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b872f14607ffb9795e38a1b5fa6256fd",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "eac95040f65f5653515e",
+    "url": "/todo/static/css/main.e2ff18da.chunk.css"
+  },
+  {
+    "revision": "822ba2ffa3bd8b72f15a",
+    "url": "/todo/static/js/2.8e858d99.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/todo/static/js/2.8e858d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac95040f65f5653515e",
+    "url": "/todo/static/js/main.4f80cf40.chunk.js"
+  },
+  {
+    "revision": "a65703e962574574e50d",
+    "url": "/todo/static/js/runtime-main.3669824a.js"
+  }
+]);
